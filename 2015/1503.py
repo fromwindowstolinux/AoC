24389dc029f338b69c66126d4899a73e3cb483e2
fromwindowstolinux <nhiroin@gmail.com>
2022-11-14 11:20:02 +0800

    part 1 done

---
+++ 2015/1503.py
@@ -0,0 +1,54 @@
+def delivery(direction):
+
+    present_delivered = 1
+    x_coordinate = 0
+    y_coordinate = 0
+    coordinates = []
+
+    for arrow in direction:
+
+        present_delivered += 1
+
+        if arrow == '>':
+            x_coordinate += 1
+        
+        elif arrow == '<':
+            x_coordinate -= 1
+
+        elif arrow == '^':
+            y_coordinate += 1
+        
+        elif arrow == 'v':
+            y_coordinate -= 1
+
+        c = x_coordinate, y_coordinate
+        coordinates.append(c)
+
+    non_dupe = []
+    nd = 0
+        
+    for pair in coordinates:    
+        if pair not in non_dupe:
+            non_dupe.append(pair)
+            nd += 1
+
+        # print(total_nd)
+    return nd
+        
+def total():
+
+    with open('1503input.txt') as file:
+
+        content = file.read()
+        total_nd = delivery(content)
+        
+        print(total_nd)
+
+    # print("present delivered:", present_delivered)
+    # return present_delivered
+
+if __name__ == "__main__":
+    total()
+    # assert delivery("^>v<") == 5
+    # assert delivery(">") == 2
+    # assert delivery("^v^v^v^v^v") == 11
