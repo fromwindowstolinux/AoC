49fdd5f3aac86f81519ea30843496bb85d8e84ed
fromwindowstolinux <nhiroin@gmail.com>
2022-11-21 12:01:21 +0800

    year 2015 day 4 done

--- 2015/1504.py
+++ 2015/1504.py
@@ -1,16 +1,31 @@
 import hashlib
 
-secret_key = "yzbqklnj"
+def AdventCoins(part1=False):
 
-part1 = 0
+    answer = 0
 
-while True:
-    encode_sk = f'{secret_key}{part1}'.encode("utf-8")
-    hash_sk = hashlib.md5(encode_sk)
-    hex_sk = hash_sk.hexdigest()
+    while True:
 
-    if hex_sk[0:5] == "00000":
-        print("Answer for Part 1:", part1)
-        break
-    
-    part1 += 1
+        encode_sk = f'{secret_key}{answer}'.encode("utf-8")
+        hash_sk = hashlib.md5(encode_sk)
+        hex_sk = hash_sk.hexdigest()
+
+        if part1:
+            if hex_sk[0:5] == "00000":
+                return answer
+            
+        else:
+            if hex_sk[0:6] == "000000":
+                return answer
+
+        answer += 1
+
+
+if __name__ == "__main__":
+
+    with open('1504input.txt') as file:
+
+        secret_key = file.read()
+        
+        print("Answer for Part 1:", AdventCoins(part1=True))
+        print("Answer for Part 2:", AdventCoins())
