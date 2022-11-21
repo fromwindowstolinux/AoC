95a4e6260b4c91b7526858210ed786dd76985054
fromwindowstolinux <nhiroin@gmail.com>
2022-11-21 15:06:46 +0800

    year 2015 day 5 done

--- 2015/1505.py
+++ 2015/1505.py
@@ -3,14 +3,24 @@ import re
 re_vowel = re.compile(r'([aeiou].*){3,}')
 re_twice = re.compile(r'(.)\1')
 re_none = re.compile(r'ab|cd|pq|xy')
+re_pair = re.compile(r'(..).*\1')
+re_repeat = re.compile(r'(.).\1')
+
 
 with open("1505input.txt") as file:
 
     content = file.readlines()
-    ctr = 0
+
+    part1 = 0
+    part2 = 0
 
     for line in content:
+    
         if (re_vowel.search(line) and re_twice.search(line) and not re_none.search(line)):
-            # print(line)
-            ctr += 1
-    print(ctr)
+            part1 += 1
+
+        if (re_pair.search(line) and re_repeat.search(line)):
+            part2 += 1
+
+    print("Answer Part 1 is:", part1)
+    print("Answer Part 2 is:", part2)
