8861ae6e33dca8f363c88ae9cdddab8a67b04cb4
fromwindowstolinux <nhiroin@gmail.com>
2022-12-01 13:55:50 +0800

    initial commit

---
+++ 2022/2201.py
@@ -0,0 +1,20 @@
+
+group = []
+total_cal = 0
+
+with open("2201input.txt") as file:
+    for line in file:
+        line = line.strip('\n')
+        if line == '':
+            group.append(total_cal)
+            total_cal = 0
+        else:
+            total_cal += int(line)
+  
+        group.sort()
+
+    part1 = group[-1]
+    part2 = group[-1] + group[-2] + group[-3] 
+
+print("Answer for Part 1:", part1)
+print("Answer for Part 2:", part2)
