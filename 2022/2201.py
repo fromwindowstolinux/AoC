66db89db50ff6081c3a4f02444a40a4dfc11ec3a
fromwindowstolinux <nhiroin@gmail.com>
2022-12-07 16:57:32 +0800

    day 1

--- 2022/2201.py
+++ 2022/2201.py
@@ -1,20 +1,21 @@
 
-group = []
-total_cal = 0
+def do():
 
-with open("2201input.txt") as file:
-    for line in file:
-        line = line.strip('\n')
-        if line == '':
-            group.append(total_cal)
-            total_cal = 0
-        else:
-            total_cal += int(line)
-  
-        group.sort()
+    group = []
+    total_cal = 0
 
-    part1 = group[-1]
-    part2 = sum(group[-3:])
+    with open("2201input.txt") as file:
+        for line in file:
+            line = line.strip('\n')
+            if line == '':
+                group.append(total_cal)
+                total_cal = 0
+            else:
+                total_cal += int(line)
 
-print("Answer for Part 1:", part1)
-print("Answer for Part 2:", part2)
+            group.sort()
+                    
+    print("Answer for Part 1:", group[-1])
+    print("Answer for Part 2:", sum(group[-3:]))
+
+do()
